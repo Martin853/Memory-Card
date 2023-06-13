c1ec0ebfbf9a1882dd2136de54a734aff18ae45a
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-06-13 21:17:27 +0200

    Created the shuffle logic of the cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Card = () => {
+export const Card = (props) => {
+  const [selected, setSelected] = useState(false);
+  const shuffleCards = props.shuffleCards;
+  const cards = props.cards;
+
+  const handleClick = () => {
+    shuffleCards(cards);
+  };
+
   return (
-    <div className=" w-60 h-60 bg-teal-600 flex justify-center items-center hover:bg-teal-700 hover:cursor-pointer transition-all ease-linear duration-200 select-none">
-      <h1 className="font-montserrat text-8xl text-white"></h1>
+    <div
+      className=" w-60 h-60 bg-teal-600 flex justify-center items-center hover:bg-teal-700 hover:cursor-pointer transition-all ease-linear duration-200 select-none"
+      onClick={handleClick}
+    >
+      <h1 className="font-montserrat text-8xl text-white">{props.title}</h1>
     </div>
   );
 };
--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from './Card';
 
 export const CardsContainer = () => {
+  const [cards, setCards] = useState(['Z', 'Q', 'C', 'D', 'A', 'B', 'M', 'E']);
+
+  const shuffleCards = (cards) => {
+    const shuffledCards = [...cards];
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledCards[i], shuffledCards[j]] = [
+        shuffledCards[j],
+        shuffledCards[i],
+      ];
+    }
+
+    setCards(shuffledCards);
+  };
+
   return (
-    <div className="w-full h-fit mb-8 bg-gray-300 flex flex-wrap justify-around py-10 gap-3"></div>
+    <div className="w-full h-fit mb-8 bg-gray-300 grid grid-cols-4 place-items-center py-10 gap-8">
+      {cards.map((title) => {
+        return <Card title={title} cards={cards} shuffleCards={shuffleCards} />;
+      })}
+    </div>
   );
 };
