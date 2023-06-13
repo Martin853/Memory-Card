4ec1b186164d3d038f440eee23039b029ef66a04
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-06-13 21:20:09 +0200

    Added the score variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Score } from './components/Score';
 import { CardsContainer } from './components/CardsContainer';
 
 export const App = () => {
+  const [score, setScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
+
   return (
     <div className=" overflow-x-hidden">
       <Header />
-      <Score />
+      <Score score={score} highscore={highscore} />
       <CardsContainer />
     </div>
   );
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export const Score = () => {
+export const Score = (props) => {
   return (
     <div className="w-screen h-14 flex justify-around items-center">
-      <h1 className="font-montserrat text-lg">Score: </h1>
-      <h1 className="font-montserrat text-lg">Highest Score: </h1>
+      <h1 className="font-montserrat text-lg">Score: {props.score}</h1>
+      <h1 className="font-montserrat text-lg">
+        Highest Score: {props.highscore}
+      </h1>
     </div>
   );
 };
