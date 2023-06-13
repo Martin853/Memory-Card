1af123776e9f81c7ff2683b4eb1c05fe8cc35b8f
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-06-13 20:42:43 +0200

    Created cards container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Score } from './components/Score';
+import { CardsContainer } from './components/CardsContainer';
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Score />
+      <CardsContainer />
     </div>
   );
 };
---
+++ src/components/CardsContainer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CardsContainer = () => {
+  return <div className="w-full h-fit bg-gray-300 flex flex-wrap"></div>;
+};
